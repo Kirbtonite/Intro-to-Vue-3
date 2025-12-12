898897d025bf21977e26da311e097d15af1711cc
Jacky Southiphonh <southiphonhjacky@gmail.com>
2025-12-12 08:32:40 -0500

    Lesson 2 finished

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+<<<<<<< Updated upstream
             // Solution
             description: 'A warm fuzzy pair of socks.' 
             // Solution
+=======
+            description: 'The socks are green, made of nylon and made in Canada.'
+>>>>>>> Stashed changes
         }
     }
 })
